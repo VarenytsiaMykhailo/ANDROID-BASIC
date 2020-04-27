7442983f2c0014a1b183017df56d6cc19ab2c11a
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-04-27 23:13:31 +0300

    Гружу 2.HelloKitty

---
+++ AlexanderKlimovLabs/2.HelloKitty/JavaVersion/app/src/main/java/com/example/hellokitty/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.hellokitty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mHelloTextView;
+    private EditText mNameEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHelloTextView = findViewById(R.id.textView);
+        mNameEditText = findViewById(R.id.editText);
+    }
+
+    public void onClick(View view) {
+        if (mNameEditText.getText().length() == 0) {
+            mHelloTextView.setText("Hello Kitty!");
+        } else {
+            mHelloTextView.setText("Привет, " + mNameEditText.getText());
+        }
+    }
+}
