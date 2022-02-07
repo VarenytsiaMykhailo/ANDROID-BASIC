f62e8296a308cb764e01ab0a1e89ee31a9d4e1cf
VarenytsiaMykhailo <52725148+VarenytsiaMykhailo@users.noreply.github.com>
2022-02-07 03:05:39 +0300

    week6. nit_rm_elements_from_listView

---
+++ SPBETU/week6/nit_rm_elements_from_listView/app/src/main/java/com/example/company/myapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.company.myapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val data = MutableList(11) { x -> "${x * x}" }
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            adapter.remove((view as TextView).text.toString())
+        }
+
+    }
+}
